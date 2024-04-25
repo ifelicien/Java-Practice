7607a3cd70182ab3b1df9904c83ac832070bfbf6
Irwin Felicien <felicienirwin@outlook.com>
2024-04-24 23:18:00 -0500

    minor changes

--- Main.java
+++ Main.java
@@ -3,11 +3,13 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
-        Employee emp = new Employee();
+        Employee emp1 = new Employee();
 
         System.out.println("Enter First Name");
         System.out.println("Enter Last Name");
         System.out.println("Enter Employee ID");
+        
+
         
     }
 }
